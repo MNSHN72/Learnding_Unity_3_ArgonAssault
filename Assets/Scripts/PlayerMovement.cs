1db19984b983440b224bc9e46fa403e21c9a3d1e
MNSHN72 <gigamilk69@gmail.com>
2021-04-24 13:35:39 +0900

    AA-player movement2-
    #tried the input system out.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontaThrow = movement.ReadValue<Vector2>().x;
     }
 }
