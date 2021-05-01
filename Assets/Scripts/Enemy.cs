a306ab66db563aa9a6ab395af1e76266d2698fcd
MNSHN72 <gigamilk69@gmail.com>
2021-05-01 16:58:16 +0900

    AA-General-
    #end of the day commit.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int pointsOnHit;
     [SerializeField] private float HP;
 
-    private Transform parent;
+    private GameObject spawnAtRuntime;
     private bool isAlive = true;
     private ScoreBoard sb;
     private void Start()
@@ -19,7 +19,7 @@ public class Enemy : MonoBehaviour
         this.gameObject.AddComponent<Rigidbody>();
         this.gameObject.GetComponent<Rigidbody>().useGravity = false;
         sb = FindObjectOfType<ScoreBoard>();
-        parent = GameObject.FindWithTag("spawnAtRuntime").transform;
+        spawnAtRuntime = GameObject.FindWithTag("spawnAtRuntime");
     }
     private void OnParticleCollision(GameObject other)
     {
@@ -51,7 +51,7 @@ public class Enemy : MonoBehaviour
     {
         ParticleSystem spawnedExplosion =
             Instantiate(particles, this.GetComponent<Transform>().position, Quaternion.identity);
-        spawnedExplosion.transform.parent = parent;
+        spawnedExplosion.transform.parent = spawnAtRuntime.transform;
     }
 
     private void ProccessScore(int points)
